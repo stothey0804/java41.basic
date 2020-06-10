773f94491ba8af608bad4c9dcd0e7df2f51356d1
stothey0804 <stothey0804@nate.com>
2020-06-10 16:47:41 +0900

    16:50

--- race/Player.java
+++ race/Player.java
@@ -2,16 +2,21 @@ package race;
 
 public class Player {
 	private String name;
-	private float record;
+	private int record;
 	
 	public void setName(String name) {
 		this.name = name;
 	}
-	public void setRecord(float record) {
+	public void setRecord(int record) {
 		this.record = record;
 	}
+	public int getRecord() {
+		return this.record;
+	}
+	
 	public void printRecord() {
 		System.out.println("이름: " + this.name + ", 기록: " + this.record);
 	}
+	
 
 }
--- race/PlayerApp.java
+++ race/PlayerApp.java
@@ -6,13 +6,24 @@ public class PlayerApp {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		Player[] player = new Player[3];
+		Player[] player = new Player[5];
 		for(int i = 0; i < player.length; i++) {
 			player[i] = new Player(); // initialize
 			System.out.println("이름을 입력하세요.");
 			player[i].setName(scan.next());
 			System.out.println("기록을 입력하세요.");
 			player[i].setRecord(scan.nextInt());
+		}
+		
+		Player temp = new Player();
+		for(int i = 0;  i < player.length ; i++) {
+			for(int j = 0; j < player.length-1; j++) {
+				if(player[j].getRecord() > player[j+1].getRecord()) {
+					temp = player[j+1];
+					player[j+1] = player[j];
+					player[j] = temp;
+				}
+			}
 		}
 		
 		for(int i = 0; i < player.length; i++) {

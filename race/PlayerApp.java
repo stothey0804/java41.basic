0d247c7720f9bc2ed1e531c3f8a9e5f61dffd9f7
stothey0804 <stothey0804@nate.com>
2020-06-10 17:14:27 +0900

    17:14

--- race/PlayerApp.java
+++ race/PlayerApp.java
@@ -6,7 +6,7 @@ public class PlayerApp {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		Player[] player = new Player[5];
+		Player[] player = new Player[3];
 		for(int i = 0; i < player.length; i++) {
 			player[i] = new Player(); // initialize
 			System.out.println("이름을 입력하세요.");
@@ -18,22 +18,35 @@ public class PlayerApp {
 //		Player temp = new Player();
 //		for(int i = 0;  i < player.length ; i++) {
 //			for(int j = 0; j < player.length-1; j++) {
-//				if(player[j].getRecord() > player[j+1].getRecord()) {
-//					temp = player[j+1];
-//					player[j+1] = player[j];
-//					player[j] = temp;
+//				if(player[j].getRecord() < player[j+1].getRecord()) {
+//					temp = player[j];
+//					player[j] = player[j+1];
+//					player[j+1] = temp;
 //				}
 //			}
 //		}
 		
 		Player[] ranking = new Player[3];
 		if(player[0].getRecord() < player[1].getRecord()) {
-			System.out.println(player[0].getName() + "이(가) "
-					+ player[1].getName() + "보다 빠르다.");
+			if(player[0].getRecord() < player[2].getRecord()) {
+				ranking[0] = player[0];
+			}else{
+				ranking[0] = player[2];
+			}
 		}else {
-			System.out.println(player[1].getName() + "이(가) "
-					+ player[0].getName() + "보다 빠르다.");
-		}				
+			if(player[1].getRecord() < player[2].getRecord()) {
+				ranking[0] = player[1];
+			}else {
+				ranking[0] = player[2];
+			}
+			
+		}
+		System.out.println(ranking[0].getName() + "가 1등이다.");
+		
+		/* *
+		 System.out.println(player[0].getName() + "이(가) "
+					+ player[1].getName() + "보다 빠르다.");
+		 * */
 		
 		for(int i = 0; i < player.length; i++) {
 			player[i].printRecord();

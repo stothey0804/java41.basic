b2e9a56d63649c955cf1b2c811c4e4d5d56c0ce2
stothey0804 <stothey0804@nate.com>
2020-06-11 16:56:05 +0900

    16:56

--- rps/RPS.java
+++ rps/RPS.java
@@ -1,33 +1,52 @@
 package rps;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class RPS {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {			
 		Scanner scan = new Scanner(System.in);
+		Random random = new Random();
 		
+		while(true) {
+			System.out.println("-------------------------------------------");
+			System.out.println("   메뉴설명   |   1. 게임시작     0. 종료");
+			System.out.println("-------------------------------------------");
+			int flag = scan.nextInt();
+			switch (flag) {
+			case 1:
+				gameStart(scan, random);
+				break;
+			case 0:
+				return;
+			}						
+		}		
+	}
+
+	private static void gameStart(Scanner scan, Random random) {
+		System.out.println("┌─────────────────────────────────────────┐");
+		System.out.println("│                 가위 바위 보                  │");
+		System.out.println("└─────────────────────────────────────────┘");
 		System.out.println("가위, 바위, 보를 고르세요.");
 		System.out.println("1: 가위 | 2: 바위 | 3: 보");
 		
 		int user = scan.nextInt();		
 		while(user < 1 || user > 3) { // exception
-			System.out.println("1,2,3 중에서 선택주세요.");
+			System.out.println("(주의) 1,2,3 중에서 선택주세요.");
 			user = scan.nextInt();
 		}
 				
-		int com = (int) ( Math.random() * 3 ) + 1;	// computer selection ( 1 ~ 3 )
-		
+		int com = random.nextInt(3) + 1;	// computer selection ( 1 ~ 3 )
 		
 		System.out.println("user의 선택: " + rpcSelect(user));
 		System.out.println("com의 선택: " + rpcSelect(com));
+		System.out.println("");
 		
 		String result = confirmResult(user, com);
 		
-		System.out.println("*********************************");
-		System.out.println(result);
-		
+		System.out.println("*******************************************");
+		System.out.println("결과 발표 : "+result);
+		System.out.println("*******************************************");
 	}
 	
 	private static String confirmResult(int user, int com) {		
